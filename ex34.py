7d62422cc7e30631b7141ad84ef18a1c8de7ef6b
Amaury <azapien@hotmail.com>
2020-03-01 20:49:55 -0600

    Accessing elements of a list

---
+++ ex34.py
@@ -0,0 +1,13 @@
+# Understanding and Accessing Elements of a list
+animals = ["bear", "python3.6", "peacock", "kangaroo", "whale", "platypus"]
+
+"""
+1.The animal at 1.
+2.The third (3rd) animal.
+3.The first (1st) animal.
+4.The animal at 3.
+5.The fifth (5th) animal.
+6.The animal at 2.
+7.The sixth (6th) animal.
+8.The animal at 4.
+"""
