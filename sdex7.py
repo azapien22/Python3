07d6cd1dd5dc819810291f340eefd358686b0e5b
Amaury Zapien <azapien@hotmail.com>
2021-06-23 21:01:55 -0500

    Study drill ex.7

---
+++ sdex7.py
@@ -0,0 +1,30 @@
+# Ex.7 Study Drill...More Printing
+# Prints text
+print("Mary had a little lamb.")
+# Prints text and fills in empty variable {}
+print("Its fleece was white as {}".format("snow"))
+# Print text
+print("And everywhere that Mary went.")
+# Print period x10
+print("." * 10) # What'd that do?
+# Each Variable is Assigned A Character 
+end1= "C"
+end2= "h"
+end3= "e"
+end4= "e"
+end5= "s"
+end6= "e"
+
+end7="B"
+end8="u"
+end9="r"
+end10="g"
+end11="e"
+end12="r"
+
+# Watch that comma at the end.
+# Try removing it to see what happens
+print(end1 + end2 +end3 + end4 + end5 + end6, end=' ')
+# Removing the comma "," the console states syntaxError : invalid Syntax line 27
+
+print(end7 + end8 + end9 + end10 + end11 + end12)
