b28e70aa55d72bc26479e0a36647a79878fd3a86
Amaury Zapien <azapien@hotmail.com>
2021-06-28 21:47:42 -0500

    Escape Sequences

---
+++ sdex10.py
@@ -0,0 +1,15 @@
+# Python 3 Escape Sequences
+
+print("line1 \ line2 \ line3")
+print("\\")
+print('\'')
+print("\"")
+print("\a")
+print("Hello \b World!")
+print("Hello \f World!")
+print("Hello \n World!")
+print("Hello \r World!")
+print("Hello \t World!")
+print("Hello \v World!")
+print("\110\145\154\154\157\40\127\157\162\154\144\41")
+print("\x48\x65\x6c\x6c\x6f\x20\x57\x6f\x72\x6c\x64\x21")
