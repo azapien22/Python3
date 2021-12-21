735c908b93d8758e3015ababb2ab0763f2981d21
Amaury Zapien <azapien@hotmail.com>
2021-12-21 11:46:36 -0600

    Added comments to explain the code

--- ex15ReadingFiles/sdex15.py
+++ ex15ReadingFiles/sdex15.py
@@ -5,18 +5,26 @@
 # The argv parameter returns the specific way the script was called (e.g. from a command line interface).
 from sys import argv
 
+# Get argv command line arguments (When running the file) and assigning it 2 variables
 script, filename = argv
 
+# Open the file (Using the filename) and assign to a variable 
 txt = open(filename)
 
+# Print out the filename and print out the content of the opened file, using read()
 print(f"Here's your {filename}:")
-
 print(txt.read())
+# Close File --- so as not to take up much computer memory
+txt.close()
 
+# Print out instructions and ask for user input using input()
 print("Type the filename again:")
-
 file_again = input("> ")
 
+# Open the file (using the filename) and assign to a variable
 txt_again = open(file_again)
 
+# Print out the content of the opened file, using read()
 print(txt_again.read())
+# Close File --- so as not to take up much computer memory
+txt_again.close()
